93671fb630ee4311ea1d4cb54a4f118e344f66d5
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-07-27 20:25:38 +0530

    fetch Data

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,9 +1,26 @@
 import React, { createContext } from "react";
+import { faker } from '@faker-js/faker';
 
 export const Cart = createContext();
 
 const Context = ({ children }) => {
-    return <Cart.Provider>{children}</Cart.Provider>;
+    const products = [...Array(20)].map(() => ({
+        id: faker.string.uuid(),
+        name: faker.commerce.productName(),
+        price: faker.commerce.price(),
+        image: faker.image.url(),
+        inStock: faker.helpers.arrayElement([0, 3, 5, 6, 7]), // Corrected method
+        fastDelivery: faker.datatype.boolean(), // Corrected property name
+        ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5]) // Corrected method
+    }));
+
+    console.log(products);
+
+    return (
+        <Cart.Provider value={{ products }}>
+            {children}
+        </Cart.Provider>
+    );
 };
 
 export default Context;

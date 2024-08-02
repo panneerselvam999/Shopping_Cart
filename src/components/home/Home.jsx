22dcaaa224469c4645dec2f57f714b99e793cb5f
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-08-02 22:35:55 +0530

    popup Add to Cart

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -39,7 +39,6 @@ const Home = () => {
 
         return sortedProducts;
     };
-    console.log(transformProducts().length);
 
 
     return (
--- src/components/navbar/AddToCart.jsx
+++ src/components/navbar/AddToCart.jsx
@@ -1,13 +1,14 @@
 // import React, { useState, useRef, useEffect } from "react";
 // import { CartState } from "../../context/Context";
 // import { MdDeleteForever } from "react-icons/md";
+// import { Link } from "react-router-dom";
 
-// // Handler hook for when Outside click dropdown close
-// let useClickOutside = (handler) => {
-//     let domNode = useRef();
+// // Custom hook to detect outside clicks and close the dropdown
+// const useOutsideClickHandler = (handler) => {
+//     const domNode = useRef();
 
 //     useEffect(() => {
-//         let maybeHandler = (event) => {
+//         const maybeHandler = (event) => {
 //             if (domNode.current && !domNode.current.contains(event.target)) {
 //                 handler();
 //             }
@@ -22,23 +23,20 @@
 
 //     return domNode;
 // };
-// // Handler hook for when Outside click dropdown close End Code====>>
 
 // const AddToCart = () => {
 //     const [isOpen, setIsOpen] = useState(false);
-//     // const [selectedOption, setSelectedOption] = useState(null);
 
-//     // const options = ["Option 1", "Option 2", "Option 3"];
-
-//     let domNode = useClickOutside(() => {
+//     const domNode = useOutsideClickHandler(() => {
 //         setIsOpen(false);
 //     });
 
-//     const toggleDropdown = () => setIsOpen(!isOpen);
-
 //     const {
 //         state: { cart },
+//         dispatch
 //     } = CartState();
+
+//     const toggleDropdown = () => setIsOpen(!isOpen);
 
 //     return (
 //         <div ref={domNode} className="relative inline-block w-fit text-left">
@@ -47,10 +45,12 @@
 //                     type="button"
 //                     className="inline-flex w-full items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 //                     onClick={toggleDropdown}
+//                     aria-expanded={isOpen}
+//                     aria-haspopup="true"
 //                 >
-//                     <img src="icon/cart.svg" alt="" className="mr-2 h-6 w-6" />
+//                     <img src="icon/cart.svg" alt="Cart icon" className="mr-2 h-6 w-6" />
 //                     <span>Cart</span>
-//                     <span className="ml-2">{cart.length}</span>
+//                     <span className="ml-2">({cart.length})</span>
 //                     <svg
 //                         className="-mr-1 ml-2 h-5 w-5"
 //                         xmlns="http://www.w3.org/2000/svg"
@@ -75,37 +75,55 @@
 //                         aria-orientation="vertical"
 //                         aria-labelledby="options-menu"
 //                     >
-//                         {cart.map((cartData) => (
-//                             <div
-//                                 key={cartData.id}
-//                                 className="my-1 flex w-full justify-between rounded-md bg-yellow-300"
-//                             >
-//                                 <div>
-//                                     <img
-//                                         src={cartData.image}
-//                                         alt={cartData.name}
-//                                         className="m-1 h-14 w-14"
-//                                     />
-//                                 </div>
-//                                 <div className="flex-grow">
-//                                     <p>{cartData.name}</p>
-//                                     <span>₹{cartData.price.split(".")[0]}</span>
-//                                 </div>
-//                                 <div className="flex items-center justify-center">
-//                                     <button className="mr-6 border-2 p-2">
-//                                         <MdDeleteForever />
-//                                     </button>
+//                         {cart.length > 0 ? (
+//                             <div>
+//                                 {cart.map(({ id, image, name, price }) => (
+//                                     <div
+//                                         key={id}
+//                                         className="my-1 flex w-full justify-between rounded-md bg-yellow-300"
+//                                     >
+//                                         <div>
+//                                             <img
+//                                                 src={image}
+//                                                 alt={name}
+//                                                 className="m-1 h-14 w-14"
+//                                             />
+//                                         </div>
+//                                         <div className="flex-grow">
+//                                             <p>{name}</p>
+//                                             <span>₹{price}</span>
+//                                         </div>
+//                                         <div className="flex items-center justify-center">
+//                                             <button
+//                                                 className="mr-6 border-2 p-2"
+//                                                 onClick={() =>
+//                                                     dispatch({
+//                                                         type: "REMOVE_FROM_CART",
+//                                                         payload: { id },
+//                                                     })
+//                                                 }
+//                                                 aria-label={`Remove ${name} from cart`}
+//                                             >
+//                                                 <MdDeleteForever />
+//                                             </button>
+//                                         </div>
+//                                     </div>
+//                                 ))}
+//                                 <div className="px-5 border-2">
+//                                     <Link to={"/cart"}
+//                                         onClick={() => setIsOpen(false)}>
+//                                         <button
+//                                             type="button"
+//                                             className="inline-flex w-fit  justify-center rounded border border-transparent bg-green-600 px-4 py-1 text-xl font-medium text-white transition-colors hover:bg-green-700"
+//                                         >
+//                                             Go to Cart
+//                                         </button>
+//                                     </Link>
 //                                 </div>
 //                             </div>
-//                         ))}
-//                         <div className="px-5">
-//                             <button
-//                                 type="submit"
-//                                 className="inline-flex w-full justify-center rounded border border-transparent bg-green-600 px-4 py-1 text-xl font-medium text-white transition-colors hover:bg-green-700"
-//                             >
-//                                 Go to Cart
-//                             </button>{" "}
-//                         </div>
+//                         ) : (
+//                             <p className="px-4 py-2 text-white">Cart is Empty!</p>
+//                         )}
 //                     </div>
 //                 </div>
 //             )}
@@ -114,11 +132,11 @@
 // };
 
 // export default AddToCart;
-
 import React, { useState, useRef, useEffect } from "react";
 import { CartState } from "../../context/Context";
 import { MdDeleteForever } from "react-icons/md";
 import { Link } from "react-router-dom";
+import SingleCart from "./SingleCart";
 
 // Custom hook to detect outside clicks and close the dropdown
 const useOutsideClickHandler = (handler) => {
@@ -150,8 +168,11 @@ const AddToCart = () => {
 
     const {
         state: { cart },
-        dispatch
+        dispatch,
     } = CartState();
+
+    console.log(cart);
+
 
     const toggleDropdown = () => setIsOpen(!isOpen);
 
@@ -193,49 +214,102 @@ const AddToCart = () => {
                         aria-labelledby="options-menu"
                     >
                         {cart.length > 0 ? (
-                            <div>
-                                {cart.map(({ id, image, name, price }) => (
-                                    <div
-                                        key={id}
-                                        className="my-1 flex w-full justify-between rounded-md bg-yellow-300"
+                            //   <div>
+                            //     {cart.map(({ id, image, name, price }) => (
+                            //       <div
+                            //         key={id}
+                            //         className="my-1 flex w-full justify-between rounded-md bg-yellow-300"
+                            //       >
+                            //         <div>
+                            //           <img src={image} alt={name} className="m-1 h-14 w-14" />
+                            //         </div>
+                            //         <div className="flex-grow">
+                            //           <p>{name}</p>
+                            //           <span>₹{price}</span>
+                            //         </div>
+                            //         <div className="flex items-center justify-center">
+                            //           <button
+                            //             className="mr-6 border-2 p-2"
+                            //             onClick={() =>
+                            //               dispatch({
+                            //                 type: "REMOVE_FROM_CART",
+                            //                 payload: { id },
+                            //               })
+                            //             }
+                            //             aria-label={`Remove ${name} from cart`}
+                            //           >
+                            //             <MdDeleteForever />
+                            //           </button>
+                            //         </div>
+                            //       </div>
+                            //     ))}
+                            //     <div className="border-2 px-5">
+                            //       <Link to={"/cart"} onClick={() => setIsOpen(false)}>
+                            //         <button
+                            //           type="button"
+                            //           className="inline-flex w-fit justify-center rounded border border-transparent bg-green-600 px-4 py-1 text-xl font-medium text-white transition-colors hover:bg-green-700"
+                            //         >
+                            //           Go to Cart
+                            //         </button>
+                            //       </Link>
+                            //     </div>
+                            //   </div>
+
+                            <div
+                                className="relative w-screen max-w-sm border border-gray-600 bg-gray-100 px-4 py-8 sm:px-6 lg:px-8"
+                                aria-modal="true"
+                                role="dialog"
+                                tabIndex="-1"
+                            >
+                                <button className="absolute end-4 top-4 text-gray-600 transition hover:scale-110" onClick={() => setIsOpen(false)}>
+                                    <span className="sr-only">Close cart</span>
+
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth="1.5"
+                                        stroke="currentColor"
+                                        className="h-5 w-5"
                                     >
-                                        <div>
-                                            <img
-                                                src={image}
-                                                alt={name}
-                                                className="m-1 h-14 w-14"
-                                            />
-                                        </div>
-                                        <div className="flex-grow">
-                                            <p>{name}</p>
-                                            <span>₹{price}</span>
-                                        </div>
-                                        <div className="flex items-center justify-center">
-                                            <button
-                                                className="mr-6 border-2 p-2"
-                                                onClick={() =>
-                                                    dispatch({
-                                                        type: "REMOVE_FROM_CART",
-                                                        payload: { id },
-                                                    })
-                                                }
-                                                aria-label={`Remove ${name} from cart`}
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M6 18L18 6M6 6l12 12"
+                                        />
+                                    </svg>
+                                </button>
+
+                                <div className="mt-4 space-y-6 ">
+
+
+                                    <ul className="space-y-4  max-h-56 overflow-y-scroll">
+
+                                        {cart.map((cartData) => (
+                                            <SingleCart cartData={cartData} key={cartData.id} dispatch={dispatch} />
+                                        ))}
+
+
+
+                                    </ul>
+
+                                    <div className="space-y-4 text-center">
+                                        <Link to={"/cart"} onClick={() => setIsOpen(false)}>
+                                            <span className="block rounded border border-gray-600 px-5 py-3 text-sm text-gray-600 transition hover:ring-1 hover:ring-gray-400"
                                             >
-                                                <MdDeleteForever />
-                                            </button>
-                                        </div>
-                                    </div>
-                                ))}
-                                <div className="px-5 border-2">
-                                    <Link to={"/cart"}
-                                        onClick={() => setIsOpen(false)}>
-                                        <button
-                                            type="button"
-                                            className="inline-flex w-fit  justify-center rounded border border-transparent bg-green-600 px-4 py-1 text-xl font-medium text-white transition-colors hover:bg-green-700"
+                                                View my cart
+                                            </span>
+                                        </Link>
+
+                                        {/* <a
+                                            href="#"
+                                            className="block rounded bg-gray-700 px-5 py-3 text-sm text-gray-100 transition hover:bg-gray-600"
                                         >
-                                            Go to Cart
-                                        </button>
-                                    </Link>
+                                            Checkout
+                                        </a> */}
+
+
+                                    </div>
                                 </div>
                             </div>
                         ) : (
